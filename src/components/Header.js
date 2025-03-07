373552876f735ca5521071c029f24423256d25ee
Kavin <kavin88701@gmail.com>
2025-03-07 23:31:51 +0530

    BAVIEFBV

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import cinemateimage from '../assests/cinemateimage.jpg'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import cinemateimage from '../assests/cinemateimage.jpg';
+import { Link,NavLink } from 'react-router-dom';
 export const Header = () => {
   return (
     <header>
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="https://www.youtube.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-              <img src={cinemateimage} className="h-8" alt="Flowbite Logo" />
+              <img src={cinemateimage} className="h-8" alt="Logo" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CINEMATE</span>
           </Link>
           <div className="flex md:order-2">
@@ -44,18 +44,21 @@ export const Header = () => {
               </div>
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                  <a to="#" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+                  <a to="/" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page" end >Home</a>
                 </li>
                 <li>
-                  <Link to="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
+                  <NavLink to="/movies/popular" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Popular</NavLink>
                 </li>
                 <li>
-                  <Link to="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</Link>
+                  <NavLink to="/movies/top" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Top rated</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/movies/upcoming" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Upcoming</NavLink>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
-</header>
+      </header>
   )
 }
