fe57c44d4f6136578afd6ab1336c51c077fb1b0c
Kavin <kavin88701@gmail.com>
2025-03-08 17:13:08 +0530

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
-         <footer>
-            <footer class="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
-                <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                  <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <a href="https://flowbite.com/" class="hover:underline">Cinemate</a>. All Rights Reserved.
-                 </span>
-                <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">Instagram</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">Youtube</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">LinkedIn</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline">Github</a>
-                    </li>
-                </ul>
-                </div>
-            </footer>
-          </footer>
+      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+              <li>
+                  <a href="https://www.instagram.com/shubham.ul/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+              </li>
+              <li>
+                  <a href="https://www.linkedin.com/in/shubhamsarda/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+              </li>
+              <li>
+                  <a href="https://twitter.com/shubham_ul" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Twitter</a>
+              </li>
+              <li>
+                  <a href="https://www.youtube.com/channel/UCuH_UB-L_PaVQSBoOqp0cqw" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Youtube</a>
+              </li>
+              <li>
+                  <a href="https://github.com/ShubhamSarda" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
+              </li>
+              
+          </ul>
+      </footer>
   )
 }
